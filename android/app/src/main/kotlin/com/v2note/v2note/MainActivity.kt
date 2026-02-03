6ec1c9990e5a3abf8d25635ae531fbd6c10f49cb
zedpa <zedpan@163.com>
2026-02-03 14:45:34 +0800

    feat: bootstrap flutter app

---
+++ android/app/src/main/kotlin/com/v2note/v2note/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.v2note.v2note
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+﻿import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const V2NoteApp());
+}
+
+class V2NoteApp extends StatelessWidget {
+  const V2NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'V2Note',
+      home: const TimelineScreen(),
+    );
+  }
+}
+
+class TimelineScreen extends StatelessWidget {
+  const TimelineScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Timeline')),
+      body: const Center(child: Text('Timeline Feed')),
+    );
+  }
+}
