4ed64e15e44b246414151de944ca1cc6cc9aa91d
zedpa <zedpan@163.com>
2026-02-03 15:17:18 +0800

    test: update widget test for V2NoteApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,4 @@
-// This is a basic Flutter widget test.
-//
+﻿// This is a basic Flutter widget test.
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
@@ -11,20 +10,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:v2note/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('timeline screen renders', (tester) async {
+    await tester.pumpWidget(const V2NoteApp());
+    expect(find.byType(MaterialApp), findsOneWidget);
+    expect(find.text('Timeline'), findsOneWidget);
   });
 }
